cac60666d4969370ca75b11f25a760ef088b7164
Anton Douginets <antondouginets@gmail.com>
2018-11-30 03:56:27 +0100

    Update octopr.m

--- src/octopr.m
+++ src/octopr.m
@@ -51,7 +51,7 @@ function [xbar, dxbar] = getwavg(x, dx)
   dxbar = sqrt(suma)/sum(weights);
 endfunction
 
-function [chi2, dof, c2d] = chi2dof(fitval, mesval, dy, nparam)
+function [chi2, dof, c2d] = chi2dof(fitval, mesval, dmesval, nparam)
   #berechne chi^2, dof, und chi^2/dof werte.
   # input: fitval - Werte aus dem fit,
   # input: mesval - Gemessene werte,
