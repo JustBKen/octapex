c1c9a63d38ec72a88afda1a25a5d0bf9159405c6
Anton Douginets <antondouginets@gmail.com>
2019-03-12 21:42:36 +0100

    fix for n-param vector functions

--- inst/NLSQ.m
+++ inst/NLSQ.m
@@ -1,19 +1,20 @@
-function [p, Vp, n, dS] = NLSQ(f, x, y, Vy, p0, stol = 0.0001, N = 50)
+function [p, Vp, n, dS, S] = NLSQ(f, x, y, Vy, p0, stol = 0.0001, N = 50)
   # Assumption: f = f(x, p)
   W = inv(Vy);  # Gewichtsmatrix
   # 1st iteration
   n = 1;
-  p = p0;
   dS = 1;
+  p = p0;
   while (dS > stol)&&(n < N + 1)
     A = Jacobi(f, x, p);
-    resid = (y .- f(x,p))';
+    resid = (y .- f(x, p))';  # spaltenvektor
     dp = inv(A'*W*A)*A'*W*resid;
     dS = dp'*A'*W*resid;
-    p += dp;  
+    p = (p' + dp)';  
     n += 1;
   endwhile
   n -= 1;
   A = Jacobi(f, x, p);
   Vp = inv(A'*W*A);
+  S = sum((y .- f(x, p)).**2);
 endfunction
