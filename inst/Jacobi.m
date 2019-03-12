231ae3dcfbfe5639c8c60c41596148e556237893
Anton Douginets <anton.douginets@physik.hu-berlin.de>
2019-03-13 00:48:39 +0100

    ok, remove that shit now
    heh

--- inst/Jacobi.m
+++ inst/Jacobi.m
@@ -2,7 +2,7 @@ function J = Jacobi(f, x, p)
   # Jacobian of f, of the form (df(xi)/dpj)ij
   J = [];
   j = 1;
-  maxj = length(p);vbfbfgbbfgbgfbgfbgfbgfbgfb
+  maxj = length(p);
   while j <= maxj
     column = pder2(f, j+1, x, p);
     J = [J; column];
