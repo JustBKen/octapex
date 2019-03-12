02f5867c8932c5eafe6e2d5a8beb6e6951735020
Anton Douginets <antondouginets@gmail.com>
2019-03-12 18:58:56 +0100

    n-arg functions fix 1

--- inst/Jacobi.m
+++ inst/Jacobi.m
@@ -4,20 +4,20 @@ function J = Jacobi(f, x, p)
   maxj = length(p);
   i = 1;  # start top
   j = 1;  # start left
-  J = pder(f, 1 + j, x(i), p(j));  # initialize first row
+  J = pder(f, 1 + j, x(i), p);  # initialize first row
   j = 2;  # step right
   while j < maxj + 1  # build first row
-    next = pder(f, j+1, x(i), p(j));  # calculate pder
+    next = pder(f, j+1, x(i), p);  # calculate pder
     J = [J, next];  # append to row
     j += 1;  # step right
   endwhile
   i = 2;  # step down
   while i < maxi + 1
     j = 1;  # start left
-    row = pder(f, j+1, x(i), p(j)); # initialize next row
+    row = pder(f, j+1, x(i), p); # initialize next row
     j += 1; # step right
     while j < maxj + 1  # build next row
-      next = pder(f, j+1, x(i), p(j));  # calculate pder
+      next = pder(f, j+1, x(i), p);  # calculate pder
       row = [row, next];  # append to row
       j += 1;  # step right
     endwhile
