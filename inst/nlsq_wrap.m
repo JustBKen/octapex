a3c8505d25c9955c54868e6d5271251d52dd78a4
Anton Douginets <anton.douginets@physik.hu-berlin.de>
2019-03-14 21:36:26 +0100

    fix weighting leasqr
    add proper treatment of uncertainties, as leasqr wants to have them

--- inst/nlsq_wrap.m
+++ inst/nlsq_wrap.m
@@ -1,6 +1,6 @@
 function [p, up, Vp, fx] = nlsq_wrap(f, x, p0, y, dy, stol = 0.0001, N = 50)
   #Wrapper for leasqr from optim
-  w = dy.**(-2);  # set weights to 1/u^2
+  w = dy.**(-1);  # set weights to 1/u (see leasqr function reference)
   [fx, p, cvg, iter, Kp, Vp] = leasqr(x, y, p0, f, stol, N, w); 
   p = p';
   up = sqrt(diag(Vp))';
